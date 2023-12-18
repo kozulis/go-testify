a40557e5dfa3faafbefd1dc6e99888439c2e1df9
Milovanov Aleksey <kozulis@yandex.ru>
2023-12-19 01:07:50 +0300

    fix:добавил общий метод, исправил недочеты

--- precode_test.go
+++ precode_test.go
@@ -8,51 +8,43 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	totalCount := 4
-	req := httptest.NewRequest("GET", "/cafe?count=55&city=moscow", nil) // здесь нужно создать запрос к сервису
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-	resp := responseRecorder.Result()
-	defer resp.Body.Close()
-	body, _ := io.ReadAll(resp.Body)
-
+	body, _, err := testRequestSettings("GET", "/cafe?count=55&city=moscow")
 	res := strings.Split(string(body), ",")
 
+	require.NoError(t, err)
 	assert.NotEmpty(t, body)
-	assert.Equal(t, totalCount, len(res))
+	assert.Len(t, res, totalCount)
 }
 
 func TestMainHandlerWhenCityIsNotAllowed(t *testing.T) {
-	req := httptest.NewRequest("GET", "/cafe?count=2&city=tver", nil)
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-	resp := responseRecorder.Result()
-	defer resp.Body.Close()
-	body, _ := io.ReadAll(resp.Body)
-
-	statusCodeBadRequest := 400
+	body, resp, err := testRequestSettings("GET", "/cafe?count=2&city=tver")
 	wrongCityErrorString := "wrong city value"
 
-	assert.Equal(t, statusCodeBadRequest, resp.StatusCode)
+	require.NoError(t, err)
+	assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
 	assert.Equal(t, wrongCityErrorString, string(body))
 }
 
 func TestMainHandlerWhenRequestIsValid(t *testing.T) {
-	req := httptest.NewRequest("GET", "/cafe?count=5&city=moscow", nil)
+	body, resp, err := testRequestSettings("GET", "/cafe?count=5&city=moscow")
+
+	require.NoError(t, err)
+	assert.NotEmpty(t, body)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+}
+
+func testRequestSettings(requestMethod string, path string) ([]byte, *http.Response, error) {
+	req := httptest.NewRequest(requestMethod, path, nil)
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 	resp := responseRecorder.Result()
 	defer resp.Body.Close()
-	body, _ := io.ReadAll(resp.Body)
-
-	statusCodeOk := 200
-
-	assert.NotEmpty(t, body)
-	assert.Equal(t, statusCodeOk, resp.StatusCode)
+	body, err := io.ReadAll(resp.Body)
+	return body, resp, err
 }
