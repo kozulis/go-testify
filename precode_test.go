74277fcf8285737ae34b0918d8004c0bf3b65156
Milovanov Aleksey <kozulis@yandex.ru>
2023-12-19 17:57:25 +0300

    fix:исправить тесты

--- precode_test.go
+++ precode_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -15,115 +14,114 @@ import (
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	totalCount := 4
 	cases := []struct {
-		count int
-		city  string
-		name  string
+		requestString string
+		statusCode    int
+		name          string
 	}{
 		{
-			count: 4,
-			city:  "moscow",
-			name:  "valid 4 cities",
+			requestString: "/cafe?count=5&city=moscow",
+			statusCode:    http.StatusOK,
+			name:          "5 cities statusOk",
 		},
 		{
-			count: 5,
-			city:  "moscow",
-			name:  "valid 5 cities",
-		},
-		{
-			count: 55,
-			city:  "moscow",
-			name:  "valid 55 cities",
+			requestString: "/cafe?count=55&city=moscow",
+			statusCode:    http.StatusOK,
+			name:          "55 cities statusOk",
 		},
 	}
 
 	for _, tc := range cases {
-		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
-			t.Parallel()
-			body, _, err := testRequestSettings("GET", fmt.Sprintf("/cafe?count=%d&city=moscow", tc.count))
+			body, resp, err := testRequestConfig("GET", tc.requestString)
 			res := strings.Split(string(body), ",")
 
 			require.NoError(t, err)
+			assert.Equal(t, resp.StatusCode, tc.statusCode)
 			assert.NotEmpty(t, body)
 			assert.Len(t, res, totalCount)
 		})
 	}
 }
 
-func TestMainHandlerWhenCityIsNotAllowed(t *testing.T) {
-	wrongCityErrorString := "wrong city value"
+func TestMainHandlerWhenWrongCountAndWrongCity(t *testing.T) {
 	cases := []struct {
-		count int
-		city  string
-		name  string
+		requestString string
+		statusCode    int
+		err           string
+		name          string
 	}{
 		{
-			count: 4,
-			city:  "thula",
-			name:  "not valid thula",
+			requestString: "/cafe?count=5&city=thula",
+			statusCode:    http.StatusBadRequest,
+			err:           "wrong city value",
+			name:          "wrong city value",
 		},
 		{
-			count: 5,
-			city:  "tver",
-			name:  "not valid tver",
+			requestString: "/cafe?count=5",
+			statusCode:    http.StatusBadRequest,
+			err:           "wrong city value",
+			name:          "city is empty",
 		},
 		{
-			count: 55,
-			city:  "lipetzk",
-			name:  "not valid lipetzk",
+			requestString: "/cafe?count=abc5&city=moscow",
+			statusCode:    http.StatusBadRequest,
+			err:           "wrong count value",
+			name:          "wrong count value",
+		},
+		{
+			requestString: "/cafe?city=moscow",
+			statusCode:    http.StatusBadRequest,
+			err:           "count missing",
+			name:          "count is empty",
 		},
 	}
 
 	for _, tc := range cases {
-		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
-			t.Parallel()
-			body, resp, err := testRequestSettings("GET", fmt.Sprintf("/cafe?count=%d&city=%s", tc.count, tc.city))
+			body, resp, err := testRequestConfig("GET", tc.requestString)
 
 			require.NoError(t, err)
 			assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
-			assert.Equal(t, wrongCityErrorString, string(body))
+			assert.Equal(t, tc.err, string(body))
 		})
 	}
 }
 
 func TestMainHandlerWhenRequestIsValid(t *testing.T) {
 	cases := []struct {
-		count int
-		city  string
-		name  string
+		count         int
+		requestString string
+		statusCode    int
+		name          string
 	}{
 		{
-			count: 4,
-			city:  "moscow",
-			name:  "valid ok 4 moscow",
+			count:         1,
+			requestString: "/cafe?count=1&city=moscow",
+			statusCode:    http.StatusOK,
+			name:          "1 city statusOk",
 		},
 		{
-			count: 2,
-			city:  "moscow",
-			name:  "valid ok 2 moscow",
-		},
-		{
-			count: 1,
-			city:  "moscow",
-			name:  "valid ok 1 moscow",
+			count:         3,
+			requestString: "/cafe?count=3&city=moscow",
+			statusCode:    http.StatusOK,
+			name:          "3 cities statusOk",
 		},
 	}
 
 	for _, tc := range cases {
-		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
-			t.Parallel()
-			body, resp, err := testRequestSettings("GET", fmt.Sprintf("/cafe?count=%d&city=moscow", tc.count))
+			body, resp, err := testRequestConfig("GET", tc.requestString)
+			res := strings.Split(string(body), ",")
 
 			require.NoError(t, err)
 			assert.NotEmpty(t, body)
 			assert.Equal(t, http.StatusOK, resp.StatusCode)
+			assert.Len(t, res, tc.count)
 		})
 	}
 }
 
-func testRequestSettings(requestMethod string, path string) ([]byte, *http.Response, error) {
+func testRequestConfig(requestMethod string, path string) ([]byte, *http.Response, error) {
 	req := httptest.NewRequest(requestMethod, path, nil)
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
